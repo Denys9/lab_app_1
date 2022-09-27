12234b8c7518ceec1042e07d40e739231c8055cf
student.lozin <lozindenis6@gmail.com>
2022-09-27 20:33:40 +0300

    Create work "№4"

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ giga = int(input("Введіть розмір файлу у гігабайтах
 inetbit = int(input("Введіть швидкість інтернету у бітах в секунду - "))
 ver = int(input(' 1 часи \n 2 хвилини \n 3 секунди \n'))
 if ver == 1:
-    r =
+    r = giga*8589934592
+    t = int(r/inetbit/60/60)
     p = 'часи'
     y = p
-    print(r)
+    print("В часах це буде - ", t)
 elif ver == 2:
-    r = num_1-num_2
+    r = giga * 8589934592
+    t = int(r / inetbit / 60)
     k = 'хвилини'
     y = k
-    print(r)
+    print("В хвилинах це буде - ", t)
 elif ver == 3:
-    r = (num_1+num_2)/2
+    r = giga * 8589934592
+    t = int(r / inetbit)
     n = 'секунди'
     y = n
-    print(r)
+    print("В секундах це буде - ",t)
