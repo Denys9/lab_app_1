156aec7373bf4587a1928cab9b5024f17e9e2756
Denys9 <113623082+Denys9@users.noreply.github.com>
2022-09-28 14:57:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,14 +19,17 @@ elif ver == 3:
     y = n
     print("До півночі залишилось ",sec_2,"секунд")
 else:
-    r = int(sec / 60 / 60)
-    hours = int(24 - r)
-    r = sec / 60
-    mins = 1440 - r
-    r = 86400 - sec
-    sec_2 = r
+    end = 86400
+    between = end - sec
+    min = 0
+    h = 0
+    print(f'Ваш час: {int(sec/3600)}:{int((sec%3600)/60)}:{int((sec%3600)%60)}')
+    if between>0:
+        h = int(between/3600)
+        min = int((between%3600)/60)
+        sec = int((between%3600)%60)
+        print(f'До півночі залишилось: {h}:{min}:{sec}')
     m = 'все число'
     y = m
-    print("До півночі залишилось ",hours,":",mins,":",sec_2,": секунд")
 
 
